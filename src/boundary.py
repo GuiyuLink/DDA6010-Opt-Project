bd9b43406c63b2620a72c4bb036ceaa618f046ee
Guang Jin <jingool2018@gmail.com>
2020-12-26 23:08:44 +0800

    comment

--- src/boundary.py
+++ src/boundary.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 class FuncClass():
-    def func1(self, x, y):
+    def func0(self, x, y):
         return 1 + np.sin(2*np.pi*x)
 
-    def func2(self, x, y):
-        return 1 + np.cos(1/x + 0.001)
+    def func1(self, x, y):
+        return 1 + np.cos(1/(x + 0.001))
 
-    def func3(self, x, y):
+    def func2(self, x, y):
         return 1/2 - np.abs(y - 1/2)
 
-    def func4(self, x, y):
+    def func3(self, x, y):
         return 1/(1+np.exp(x*y))
 
-    def func5(self, x, y):
+    def func4(self, x, y):
         return 1 + np.arcsin(-1+2*np.sqrt(x*y))
--- src/main.py
+++ src/main.py
@@ -1,40 +1,69 @@
-from problem_wrap import Problem
-import numpy as np 
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import FormatStrFormatter, LinearLocator
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+from BB import BB_gradient, gradient_method_backtracking
 from boundary import FuncClass
 from newton import globalized_newton
-from BB import gradient_method_backtracking, BB_gradient
+from problem_wrap import Problem
+
 
 def plot3D(m, n, Z):
-    from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
-    import matplotlib.pyplot as plt
-    from matplotlib import cm
-    from matplotlib.ticker import LinearLocator, FormatStrFormatter
-    import numpy as np
     x = np.linspace(0, 1, n)
     y = np.linspace(0, 1, m)
-
     X, Y = np.meshgrid(x, y)
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-    plt.show()
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+# method: 0: basic GD with backtracking, 1: globalized Newton method,  2: L-BFGS
+# 3: nonmonotone line search procedures, 4: Barzilai-Borwein steps 5: inertial techniques and momentum
+# 6: compact representation of the L-BFGS update
+METHOD = 1
+func_num = 1
+FUNC = 'func_class.func{}'.format(func_num)
+OPT = [2.067380905151367, None, None, None, None][func_num]
 
 if __name__ == "__main__":
-    m, n = 80, 80
-    tol = 1e-5
+    # parameters
+    m, n = 100, 100
+    tol = 1e-7
     s, sigma, gamma = 1, 0.5, 0.1
     beta1 = beta2 = 1e-6
     p = 0.1
+
     func_class = FuncClass()
-    problem = Problem(func_class.func5, m, n)
+    problem = Problem(eval(FUNC), m, n)
+
+    # initial
     # x = np.random.randn((m-2)*(n-2))
     x = np.zeros((m-2)*(n-2))
-    points, iterations = (globalized_newton(problem.obj, problem.grad, problem.hess, x,  tol, s, sigma, gamma, beta1, beta2, p))
-    # print(gradient_method_backtracking(problem.obj, problem.grad, x, tol, s, sigma, gamma))
-    # points, iterations = (BB_gradient(problem.obj, problem.grad, x, tol, s, sigma, gamma))
+
+    # algorithm
+    if METHOD == 0:
+        points, iterations = gradient_method_backtracking(problem.obj, problem.grad, x, tol, s, sigma, gamma)
+    elif METHOD == 1:
+        points, iterations = (globalized_newton(problem.obj, problem.grad, problem.hess, x,  tol, s, sigma, gamma, beta1, beta2, p))
+    elif METHOD == 2:
+        pass 
+    elif METHOD == 3:
+        pass 
+    elif METHOD == 4:
+        points, iterations = (BB_gradient(problem.obj, problem.grad, x, tol, s, sigma, gamma))
+    elif METHOD == 5:
+        pass 
+    elif METHOD == 6:
+        pass 
+
+    # # plot gap
+    # gap = []
+    # for x in points:
+    #     obj = problem.obj(x)
+    #     gap.append(np.abs(obj - OPT) / max(1, np.abs(OPT)))
+    # plt.plot(gap)
 
     # plot
     inputs = np.empty((m, n))
@@ -43,3 +72,5 @@ if __name__ == "__main__":
     inputs[1:-1, 1:-1] = x
     problem.model.bound_constrain(inputs)
     plot3D(m,n,inputs)
+    plt.savefig('../report/{}_method{}.pdf'.format(FUNC, METHOD))
+    plt.show()
